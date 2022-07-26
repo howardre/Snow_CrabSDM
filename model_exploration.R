d97c7ee1778a0e4de3a1eef48dcb58ecfbd43751
Rebecca Howard <howardrebecca@gmail.com>
2022-07-26 11:37:56 -0700

    Add more brt code

--- model_exploration.R
+++ model_exploration.R
@@ -42,6 +42,22 @@ rf_females1 <- randomForest(scaled_female ~ depth +
                             proximity = T)
 print(rf_females1)
 
+# Males
+set.seed(1993)
+rf_males1 <- randomForest(scaled_male ~ depth +
+                            phi +
+                            ice +
+                            sst +
+                            doy +
+                            male_mature +
+                            male_immature,
+                          data = na.exclude(crab_train),
+                          ntree = 1000,
+                          mtry = 5,
+                          importance = T,
+                          proximity = T)
+print(rf_females1)
+
 
 # Boosted regression trees ----
 # Females
@@ -50,6 +66,85 @@ brt_females1 <- gbm.step(data = crab_train,
                          gbm.y = 36,
                          family = 'gaussian',
                          tree.complexity = 5,
+                         learning.rate = 0.05,
+                         bag.fraction = 0.5) # probably producing too many trees
+summary(brt_females1) 
+
+brt_females2 <- gbm.step(data = crab_train,
+                         gbm.x = c(9, 26, 29:32, 35),
+                         gbm.y = 36,
+                         family = 'gaussian',
+                         tree.complexity = 5,
                          learning.rate = 0.01,
-                         bag.fraction = 0.5)
-summary(brt_females1)
+                         bag.fraction = 0.5) # 
+summary(brt_females2)
+
+brt_females3 <- gbm.step(data = crab_train,
+                         gbm.x = c(9, 26, 29:32, 35),
+                         gbm.y = 36,
+                         family = 'gaussian',
+                         tree.complexity = 5,
+                         learning.rate = 0.01,
+                         bag.fraction = 0.5) # 
+summary(brt_females3)
+
+# Attempt dropping variable
+females_simp <- gbm.simplify(brt_females1, n.drops = 5)
+summary(females_simp)
+
+# Choose final model
+females_final <- females_simp # Change this once decision made
+
+# Plot the variables
+gbm.plot(females_final,
+         n.plots = 7,
+         plot.layout = c(3, 4),
+         write.title = F,
+         smooth = T,
+         common.scale = T,
+         cex.axis = 1.7,
+         cex.lab = 1.7,
+         lwd = 1.5)
+
+# Plot the fits
+females_int <- gbm.interactions(females_final)
+females_int$interactions
+
+par(mfrow = c(1, 3))
+gbm.perspec(females_final,
+            2, 3,
+            z.range = c(0, 7.69),
+            theta = 60,
+            col = "light blue",
+            cex.axis = 0.8,
+            cex.lab = 1,
+            ticktype = "detailed")
+
+gbm.perspec(females_final,
+            1, 3,
+            z.range = c(0, 10.75),
+            theta = 60,
+            col = "light blue",
+            cex.axis = 0.8,
+            cex.lab = 1,
+            ticktype = "detailed")
+
+gbm.perspec(females_final,
+            1, 2,
+            z.range = c(0, 9.45),
+            theta = 60,
+            col = "light blue",
+            cex.axis = 0.8,
+            cex.lab = 1,
+            ticktype = "detailed")
+
+
+# Males
+brt_males1 <- gbm.step(data = crab_train,
+                       gbm.x = c(9, 26, 29:31, 33:34),
+                       gbm.y = 37,
+                       family = 'gaussian',
+                       tree.complexity = 5,
+                       learning.rate = 0.05,
+                       bag.fraction = 0.5)
+summary(brt_males1)
