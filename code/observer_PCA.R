53c15b0a0a453b5ec257f2d7f21138070817762c
Rebecca Howard <howardrebecca@gmail.com>
2022-11-15 15:57:22 -0800

    Change to pca of life stages

--- code/observer_PCA.R
+++ code/observer_PCA.R
@@ -18,71 +18,58 @@ observer_df$index <- 1:nrow(observer_df)
 observer_df$lncpue_male_legal <- log(observer_df$tot_legal + 1)
 observer_df$lncpue_male_sublegal <- log(observer_df$sublegal + 1)
 observer_df$lncpue_female <- log(observer_df$female + 1)
-legal_male_df <- na.omit(select(observer_df, year, doy, tot_legal, latitude, longitude, depth))
-sublegal_male_df <- na.omit(select(observer_df, year, doy, sublegal, latitude, longitude, depth))
-female_df <- na.omit(select(observer_df, year, doy, female, latitude, longitude, depth))
-
-# Add index for haul ID
-legal_male_df$index <- 1:nrow(legal_male_df)
-legal_male_df$anomalies <- legal_male_df$tot_legal - mean(legal_male_df$tot_legal)
+observer_filtered <- select(observer_df, lncpue_male_legal, lncpue_male_sublegal, lncpue_female)
 
 # Create matrices
-legal_male <- as.data.frame(legal_male_df[order(legal_male_df$index), ])
-legal_male_mat <- fossil::create.matrix(legal_male,
-                                        tax.name = "index",
-                                        locality = "doy",
-                                        time.col = NULL,
-                                        time = NULL,
-                                        abund = T,
-                                        abund.col = "anomalies")
+corrplot(cor(observer_filtered), is.corr = FALSE)
+observer_scaled <- scale(observer_filtered)
+
+# legal_male <- as.data.frame(legal_male_df[order(legal_male_df$index), ])
+# legal_male_mat <- fossil::create.matrix(legal_male,
+#                                         tax.name = "index",
+#                                         locality = "doy",
+#                                         time.col = NULL,
+#                                         time = NULL,
+#                                         abund = T,
+#                                         abund.col = "anomalies")
 
 # PCA
-legal_male_pca <- prcomp(legal_male_mat,
-                         scale = F,
-                         center = F)
-summary(legal_male_pca)
-plot(summary(legal_male_pca)$importance[2, ], 
-     type = "h", 
-     ylab = "Proportion of variance", 
-     xlab = "PC")
-barplot(-legal_male_pca$rotation[, 1],
-        col = "red",
-        ylim= range(-legal_male_pca$rotation[, 1]),
-        main = "Legal Males",
-        xlab = "doy",
-        ylab = "Magnitude",
-        las = 2)
+pca <- prcomp(observer_scaled,
+              scale = F,
+              center = F)
+summary(pca)
+
+pca$sdev # eigenvalues
+pca$x[, 1][1:10] # first 10 values of pca 1
+pca$rotation[, 1] # loadings for first pca
+
+# Checks
+pca1_1 <- pca$rotation[, 1]%*%observer_scaled[1, ]
+pca1_1;pca$x[1, ][1]
+
+# Plots
+plot(pca$sdev, 
+     type = 'h',
+     ylab = 'Standard Deviation',
+     xlab = 'PC')
+plot(pca$x[, 1],
+     xlab = 'Values',
+     ylab = 'PC1',
+     type = 'b')
+plot(pca$rotation[, 1],
+     ylab = 'Loadings',
+     xlab = 'Life Stages')
 
 # Try with vegan
-legal_male_pca <- princomp(legal_male_mat, cor = F)
-summary(legal_male_pca, loadings = T)
-head(summary(legal_male_pca))
+vegan_pca <- princomp(observer_scaled, cor = TRUE)
+summary(vegan_pca)
+head(summary(vegan_pca))
 
-biplot(legal_male_mat)
+vegan_pca$sdev
+vegan_pca$loadings[, 1]
+vegan_pca$scores[, 1]
+vegan_pca$scale
+vegan_pca$center
 
-# Outlier evaluation
-legal_male_df <- filter(legal_male_df, depth < 400, tot_legal > 0)
-sublegal_male_df <- filter(sublegal_male_df, depth < 400, sublegal > 0)
-female_df <- filter(female_df, depth < 400, female > 0)
-
-
-# PCA
-variables <- c("year", "doy", "latitude", "depth")
-
-legal_male_pca <- rda(legal_male_df[, variables], scale = T)
-biplot(legal_male_pca, display = "species", scaling = "species")
-head(summary(legal_male_pca))
-legal_male_loadings <- scores(legal_male_pca, display = "species", scaling = 0)
-sort(abs(legal_male_loadings[, 1]), decreasing = T)
-
-sublegal_male_pca <- rda(sublegal_male_df[, variables], scale = T)
-biplot(sublegal_male_pca)
-head(summary(sublegal_male_pca))
-sublegal_male_loadings <- scores(sublegal_male_pca, display = "species", scaling = 0)
-sort(abs(sublegal_male_loadings[, 1]), decreasing = T)
-
-female_pca <- rda(female_df[, variables], scale = T)
-biplot(female_pca)
-head(summary(female_pca))
-female_loadings <- scores(female_pca, display = "species", scaling = 0)
-sort(abs(female_loadings[, 1]), decreasing = T)
+# Plot
+biplot(vegan_pca)
