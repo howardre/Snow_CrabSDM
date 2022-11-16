405084f6bb6410bb0f287570f8947c8c110182d9
Rebecca Howard <howardrebecca@gmail.com>
2022-11-16 13:48:45 -0800

    Match to survey data

--- code/observer_PCA.R
+++ code/observer_PCA.R
@@ -10,20 +10,22 @@ library(dplyr)
 # Functions ----
 
 # Load data ----
-observer_df <- readRDS(here('data/Snow_CrabData', 'observer_df.rds'))
+observer_df <- readRDS(here('data/Snow_CrabData', 'observer_summarized.rds'))
 bycatch_df <- readRDS(here('data/Snow_CrabData', 'bycatch_df.rds'))
+crab_summary <- readRDS(here('data/Snow_CrabData', 'crab_summary.rds'))
 
 # Filter to groups
-observer_df$index <- 1:nrow(observer_df)
-observer_df$lncpue_male_legal <- log(observer_df$tot_legal + 1)
-observer_df$lncpue_male_sublegal <- log(observer_df$sublegal + 1)
-observer_df$lncpue_female <- log(observer_df$female + 1)
+observer_df$lncpue_male_legal <- log(observer_df$obs_male_legal + 1)
+observer_df$lncpue_male_sublegal <- log(observer_df$obs_male_sub + 1)
+observer_df$lncpue_female <- log(observer_df$obs_female + 1)
+
+# Average values for the stations and years
 
 # Make matrices with year and station ID for each group
 # Sites as rows, years as columns?
 legal_male_mat <- fossil::create.matrix(as.data.frame(observer_df),
                                         tax.name = "STATIONID",
-                                        locality = "year",
+                                        locality = "year_lag",
                                         time.col = NULL,
                                         time = NULL,
                                         abund = T,
@@ -31,7 +33,7 @@ legal_male_mat <- fossil::create.matrix(as.data.frame(observer_df),
 
 sublegal_male_mat <- fossil::create.matrix(as.data.frame(observer_df),
                                            tax.name = "STATIONID",
-                                           locality = "year",
+                                           locality = "year_lag",
                                            time.col = NULL,
                                            time = NULL,
                                            abund = T,
@@ -39,7 +41,7 @@ sublegal_male_mat <- fossil::create.matrix(as.data.frame(observer_df),
 
 female_mat <- fossil::create.matrix(as.data.frame(observer_df),
                                         tax.name = "STATIONID",
-                                        locality = "year",
+                                        locality = "year_lag",
                                         time.col = NULL,
                                         time = NULL,
                                         abund = T,
@@ -115,16 +117,23 @@ female_pca$scale
 female_pca$center
 
 # Plot
-biplot(legal_male_pca, 
-       xlim = c(-0.1, 0.42),
-       ylim = c(-0.8, 0.1))
-
-biplot(sublegal_male_pca,
-       xlim = c(-0.1, 0.5))
-
-biplot(female_pca, 
-       xlim = c(-0.15, 0.7),
-       ylim = c(-0.7, 0.25))
+biplot(legal_male_pca)
+biplot(sublegal_male_pca)
+biplot(female_pca)
 
 ## Match to data frame
+legal_male_loadings <- as.data.frame(legal_male_pca$loadings[, 1])
+legal_male_loadings <- tibble::rownames_to_column(legal_male_loadings, "year")
+colnames(legal_male_loadings)[2] <- "legal_male_loadings"
 
+sublegal_male_loadings <- as.data.frame(sublegal_male_pca$loadings[, 1])
+sublegal_male_loadings <- tibble::rownames_to_column(sublegal_male_loadings, "year")
+colnames(sublegal_male_loadings)[2] <- "sublegal_male_loadings"
+
+female_loadings <- as.data.frame(female_pca$loadings[, 1])
+female_loadings <- tibble::rownames_to_column(female_loadings, "year")
+colnames(female_loadings)[2] <- "female_loadings"
+
+crab_summary$legal_male_loading <- legal_male_loadings$legal_male_loadings[match(crab_summary$year, legal_male_loadings$year)]
+crab_summary$sublegal_male_loading <- sublegal_male_loadings$sublegal_male_loadings[match(crab_summary$year, sublegal_male_loadings$year)]
+crab_summary$female_loading <- female_loadings$female_loadings[match(crab_summary$year, female_loadings$year)]
