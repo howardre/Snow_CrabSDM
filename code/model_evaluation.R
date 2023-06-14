ba0219d9723c219f7a88cc8dfb8df8406a0a754f
Rebecca Howard <howardrebecca@gmail.com>
2023-06-14 16:29:23 -0700

    Finalize code for shap calculations

--- code/model_evaluation.R
+++ code/model_evaluation.R
@@ -15,7 +15,8 @@ library(mapdata)
 library(fields)
 library(ggplot2)
 library(enmSdmX) # use for grid search, wrapper for dismo
-library(kernelshap)
+library(fastshap)
+library(mshap)
 library(shapviz)
 source(here('code/functions', 'vis_gam_COLORS.R'))
 source(here('code/functions', 'distance_function.R'))
@@ -504,6 +505,40 @@ brt_deviance <- function(brt){
   deviance <- (brt$model$self.statistics$mean.null - brt$model$cv.statistics$deviance.mean) /
     brt$model$self.statistics$mean.null
   return(deviance)
+}
+
+calculate_shap <- function(brt_abun, brt_pres, data){
+  explain <- data[vars] # contains just variables to calculate SHAP for
+  
+  # Baseline/expected values
+  abun_baseline = mean(pred_fun(brt_abun$model,
+                                newdata = explain))
+  pres_baseline = mean(pred_fun(brt_pres$model, 
+                                newdata = explain))
+  
+  # Calculate individual model SHAP values
+  set.seed(1993)
+  abun_shap <- fastshap::explain(brt_abun$model, 
+                                 X = explain,
+                                 pred_wrapper = pred_fun,
+                                 nsim = 50,
+                                 shap_only = FALSE,
+                                 adjust = TRUE)
+  pres_shap <- fastshap::explain(brt_pres$model,
+                                 X = explain,
+                                 pred_wrapper = pred_fun,
+                                 nsim = 50,
+                                 shap_only = FALSE,
+                                 adjust = TRUE)
+  
+  # Combine SHAP values
+  mshap <- mshap(shap_1 = abun_shap$shapley_values,
+                 shap_2 = pres_shap$shapley_values,
+                 ex_1 = abun_baseline,
+                 ex_2 = pres_baseline)
+  
+  # Return list of all three types of SHAP values
+  return(list(abun_shap, pres_shap, mshap))
 }
 
 # GAMs ----
@@ -1412,111 +1447,22 @@ mat_female_names <- c("depth", "temperature", "phi", "ice_mean", "longitude",
 imm_female_names <- c("depth", "temperature", "phi", "ice_mean", "longitude",
                       "latitude", "julian", "female_loading_station",
                       "bcs_immature_female", "log_pcod_cpue")
+pred_fun <- function(X_model, newdata){
+  predict.gbm(X_model, newdata)
+}
+
 ## Legal Males ----
-### TreeSHAP ----
-library(treeshap)
-library(waterfalls)
 leg_male_data <- crab_trans %>% # can use full data set
   dplyr::select(depth, temperature, phi, ice_mean, bcs_legal_male,
                 longitude, latitude, julian, legal_male_loading,
                 log_pcod_cpue, lncount_leg_male, legal_male, 
                 pres_leg_male, year_f, year, legal_male_loading_station) %>%
-  tidyr::drop_na(lncount_leg_male, ice_mean)
-leg_male_explain <- leg_male_data[vars] %>% tidyr::drop_na() # cannot use year factor unless hot coded
-leg_male_gbm <- gbm.unify(brt_leg_male_abun$model, leg_male_explain)
-leg_male_gbm_abun <- treeshap(leg_male_gbm, leg_male_explain)
-plot_contribution(leg_male_gbm_abun)
-leg_male_sv <- shapviz(leg_male_gbm_abun)
+  tidyr::drop_na(lncount_leg_male, ice_mean) # full data set with just important variables
 
-sv_importance(leg_male_sv)
-sv_importance(leg_male_sv, kind = "bee") # Use for explaining SHAP values, overall not as useful
-sv_waterfall(leg_male_sv, 1) # one observation
-sv_force(leg_male_sv, 3) # one observation
-sv_dependence(leg_male_sv, 
-              v = "temperature", 
-              color_var = "ice_mean") # specific variable relationships
-sv_dependence(leg_male_sv, 
-              v = leg_male_names, 
-              color_var = NULL,
-              color = "#6666CC30")
+leg_male_shaps <- calculate_shap(brt_leg_male_abun, brt_leg_male_base, leg_male_data)
+saveRDS(leg_male_shaps, file = here('data', 'leg_male_shaps.rds'))
 
-# Try waterfall split out by year
-leg_male_shap <- leg_male_gbm_temp$shaps
-leg_male_df <- cbind(leg_male_shap, year = leg_male_data[, 14])
-leg_male_final <- leg_male_df[, c(11, 1:10)]
-leg_male_waterfall <- leg_male_final %>% 
-  group_by(year) %>%
-  summarise(across(everything(), list(sum)))
-
-waterfall(leg_male_waterfall)
-ggplot(leg_male_df, aes(x = temperature, y = temp_shap)) +
-  geom_point(aes(color = ice_mean))
-
-### mSHAP ----
-library(mshap)
-library(shapper)
-library(DALEX)
-library(r2pmml)
-
-# May need to specify using anaconda if on Windows machine before attempting to use shap package
-# reticulate::use_condaenv("C:/Users/howar/anaconda3")
-leg_male_data <- crab_train %>% # can use full data set
-  dplyr::select(depth, temperature, phi, ice_mean, bcs_legal_male,
-                longitude, latitude, julian, legal_male_loading,
-                log_pcod_cpue, lncount_leg_male, legal_male, 
-                pres_leg_male, year_f, year, legal_male_loading_station) %>%
-  tidyr::drop_na(lncount_leg_male, ice_mean)
-leg_male_explain <- leg_male_data[vars]
-leg_male_pres <- leg_male_data[, 13]
-leg_male_abun <- leg_male_data[, 11]
-
-# Convert to model usable in treeshap
-leg_male_gbm_abun <- gbm.unify(brt_leg_male_abun$model, leg_male_explain)
-leg_male_gbm_pres <- gbm.unify(brt_leg_male_base$model, leg_male_explain)
-
-# Calculate SHAP values for each model
-leg_male_tshap_abun <- treeshap(leg_male_gbm_abun, leg_male_explain)
-leg_male_tshap_pres <- treeshap(leg_male_gbm_pres, leg_male_explain,)
-
-leg_male_abun_sv <- shapviz(leg_male_tshap_abun)
-leg_male_pres_sv <- shapviz(leg_male_tshap_pres)
-leg_male_msv <- mshapviz(c(leg_male_abun_sv, leg_male_pres_sv))
-sv_waterfall(leg_male_abun_sv, 50)
-sv_force(leg_male_pres_sv, 50)
-sv_dependence(leg_male_abun_sv, 
-              v = "temperature", 
-              color_var = "ice_mean")
-
-test_baseline <- get_baseline(leg_male_tshap_abun)
-test <- get_shap_values(leg_male_abun_sv)
-
-# fastshap
-library(fastshap)
-pred_fun <- function(X_model, newdata){
-  predict.gbm(X_model, newdata)
-}
-
-leg_male_abun_baseline = mean(pred_fun(brt_leg_male_abun$model, 
-                                       newdata = leg_male_explain))
-
-shap <- fastshap::explain(brt_leg_male_abun$model, 
-                          X = leg_male_explain, 
-                          pred_wrapper = pred_fun, 
-                          nsim = 50,
-                          shap_only = FALSE,
-                          adjust = TRUE)
-
-leg_male_abun_shap <- leg_male_tshap_abun$shaps
-leg_male_pres_shap <- leg_male_tshap_pres$shaps
-
-leg_male_mshap <- mshap(shap_1 = leg_male_abun_shap,
-                        shap_2 = leg_male_pres_shap,
-                        ex_1 = leg_male_abun_baseline, 
-                        ex_2 = 0)
-
-mshap::summary_plot(variable_values = leg_male_explain,
-                    shap_values = leg_male_mshap$shap_vals)
-
+# Visualize
 leg_male_shaps <- as.matrix(leg_male_mshap$shap_vals)
 leg_male_mshap_sv <- shapviz(leg_male_shaps, X = leg_male_explain)
 sv_importance(leg_male_mshap_sv)
@@ -1525,15 +1471,19 @@ sv_importance(leg_male_mshap_sv, kind = "bee")
 sv_dependence(leg_male_mshap_sv, 
               v = "temperature", 
               color_var = "ice_mean")
-sv_dependence(leg_male_mshap_sv, v = leg_male_names)
+sv_dependence(leg_male_mshap_sv, 
+              v = "legal_male_loading_station", 
+              color_var = "depth")
 
-### KernelSHAP ----
-leg_male_explain <- leg_male_train[vars] # only use columns in model
-leg_male_x <- leg_male_explain[sample(nrow(leg_male_explain), 500), ]
-leg_male_shap_abun <- kernelshap(brt_leg_male_abun$model, 
-                                 leg_male_explain, 
-                                 bg_X = leg_male_x)
-saveRDS(leg_male_shap_abun, file = here('data', 'leg_male_shap_full.rds'))
+sv_dependence(leg_male_mshap_sv, 
+              v = leg_male_names,
+              color_var = NULL)
+
+leg_male_pres_sv <- shapviz(leg_male_pres_shap)
+sv_importance(leg_male_pres_sv, kind = "bee")
+sv_dependence(leg_male_pres_sv, 
+              v = "temperature", 
+              color_var = "ice_mean")
 
 # Visualize
 leg_male_sv <- shapviz(leg_male_shap_abun)
