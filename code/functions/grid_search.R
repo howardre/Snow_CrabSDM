544f34e58727e93dc21cb2bab05f9180bc0bbad8
Rebecca Howard <howardrebecca@gmail.com>
2023-07-12 09:22:32 -0700

    Change max trees

--- code/functions/grid_search.R
+++ code/functions/grid_search.R
@@ -7,7 +7,7 @@ grid_search <- function(data, response, family){
            learningRate = c(0.01, 0.05, 0.1),
            bagFraction = c(0.25, 0.5, 0.75),
            minTrees = 1000, # recommended minimum by Elith
-           maxTrees = 2000,
+           maxTrees = 2500,
            cores = 6, # increase speed
            out = c('model', 'tuning')) # should return model and table with hyperparameters
 }
