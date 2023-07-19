cb7fbe89e84a5bec048d4e33744f7093a962c580
Rebecca Howard <howardrebecca@gmail.com>
2023-07-18 17:08:16 -0700

    Update map figure code

--- code/intro_map.R
+++ code/intro_map.R
@@ -1,13 +1,26 @@
 # Libraries
-library(marmap)
 library(ggOceanMaps)
-library(ggplot2)
-library(grid)
+library(here)
 
-# ggOceanMaps method
+# Create palettes and data frames
 bathy_palette <- colorRampPalette(c("lightsteelblue1", "lightsteelblue4"))(9)
 BS_bathy <- data.frame(lon = c(-176.6, -158.5),
-                       lat = c(51.5, 65.5))
+                       lat = c(51.5, 65))
+text_labels <- data.frame(place = c("Pribilof Islands", "St. Matthew Island", 
+                                    "St. Lawrence Island", "Norton Sound"),
+                          lat = c(56.2, 60, 62.7, 64),
+                          lon = c(-169.5, -172, -169.5, -164))
+text_sf <- sf::st_as_sf(text_labels, 
+                        coords = c("lon", "lat"),
+                        crs = sf::st_crs(4326))
+
+alaska_label <- data.frame(place = "Alaska",
+                           lat = 62,
+                           lon = -158)
+alaska_sf <- sf::st_as_sf(alaska_label, 
+                          coords = c("lon", "lat"),
+                          crs = sf::st_crs(4326))
+# Make map
 bering_map <- basemap(data = BS_bathy,
                       bathymetry = TRUE,
                       rotate = TRUE,
@@ -21,43 +34,38 @@ bering_map <- basemap(data = BS_bathy,
                fill = NA) +
   scale_fill_manual(values = bathy_palette) +
   labs(x = "Longitude",
-       y = "Latitude") +
-  
+       y = "Latitude")  +
+  ggspatial::annotation_north_arrow(location = "tr",
+                                    which_north = "true",
+                                    style = ggspatial::north_arrow_nautical(text_family = "serif"),
+                                    height = unit(2.5, "cm"),
+                                    width = unit(2.5, "cm")) +
+  ggspatial::annotation_scale(location = "bl",
+                              text_family = "serif",
+                              style = "ticks",
+                              height = unit(0.5, "cm"),
+                              text_cex = 1.3,
+                              line_width = 1.5) +
+  geom_sf_text(data = text_sf,
+               aes(label = place),
+               size = 5,
+               family = "serif") +
+  geom_sf_text(data = alaska_sf,
+               aes(label = place),
+               size = 7,
+               family = "serif",
+               fontface = "bold") +
+  theme(axis.text = element_text(family = "serif", size = 14),
+        axis.title = element_text(family = "serif", size = 18),
+        strip.text = element_text(family = "serif", size = 18))
 
-bering_map + 
-  annotation_north_arrow() + # not working right now, may need to update packages
-  annotation_scale()
-
-
-# marmap method
-BS_bathy <- getNOAA.bathy(lon1 = -176.5, lon2 = -156.5, 
-                          lat1 = 51, lat2 = 65.5, 
-                          resolution = 1)
-blues <- c("lightsteelblue4", "lightsteelblue3", "lightsteelblue2", "lightsteelblue1")
-greys <- c(grey(0.6), grey(0.93), grey(0.99))
-
-windows(width = 17,
-        height = 14,)
-par(mfrow = c(1, 1),
-    family = 'serif',
-    mar = c(4, 5, 3, .2) + .15)
-plot.bathy(BS_bathy,
-           image = T,
-           axes = T,
-           lwd = 0.03,
-           land = T,
-           n = 0, 
-           bpal = list(c(0,
-                         max(BS_bathy),
-                         greys),
-                       c(min(BS_bathy),
-                         0,
-                         blues)),
-           xlim = c(-176.5, -156.8),
-           ylim = c(51, 65.5),
-           ylab = "Latitude °N",
-           xlab = "Longitude °W",
-           main = "",
-           cex.lab = 2,
-           cex.main = 2.5,
-           cex.axis = 1.8)
+# Save map
+bering_map 
+dev.copy(jpeg,
+         here('results',
+              'intro_map.jpg'),
+         height = 10,
+         width = 12,
+         res = 200,
+         units = 'in')
+dev.off()
