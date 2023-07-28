7df073f3099326b98d96d683e46039e18e80f9c0
Rebecca Howard <howardrebecca@gmail.com>
2023-07-28 12:47:44 -0700

    Add step size, change tryBy

--- code/functions/grid_search.R
+++ code/functions/grid_search.R
@@ -6,9 +6,10 @@ grid_search <- function(data, response, family){
            treeComplexity = c(1, 5, 10),
            learningRate = c(0.01, 0.05, 0.1),
            bagFraction = c(0.25, 0.5, 0.75),
+           step.size = 25,
            minTrees = 1000, # recommended minimum by Elith
            maxTrees = 2500,
-           tryBy = c('learningRate', 'treeComplexity', 'bagFraction', 'numTrees'),
+           tryBy = c('learningRate', 'treeComplexity', 'bagFraction'),
            cores = 6, # increase speed,
            out = c('tuning', 'model')) # should return model and table with hyperparameters
 }
