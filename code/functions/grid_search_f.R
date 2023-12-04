6d9ea31e64e984da1829b1910a6e2b752e45e7d5
Rebecca Howard <howardrebecca@gmail.com>
2023-12-04 12:34:50 -0800

    Add grid search for forecasting

---
+++ code/functions/grid_search_f.R
@@ -0,0 +1,15 @@
+grid_search_f <- function(data, response, family){
+  trainBRT(data = data,
+           preds = c(1:7, 12),
+           resp = response,
+           family = family,
+           treeComplexity = c(1, 5, 10),
+           learningRate = c(0.01, 0.05, 0.1),
+           bagFraction = c(0.25, 0.5, 0.75),
+           step.size = 25,
+           minTrees = 1000, # recommended minimum by Elith
+           maxTrees = 2500,
+           tryBy = c('learningRate', 'treeComplexity', 'bagFraction'),
+           cores = 6, # increase speed,
+           out = c('tuning', 'model')) # should return model and table with hyperparameters
+}
